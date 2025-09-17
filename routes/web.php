48e6111e4fe2bb58b68c18c89dfd3bcba1add062
gustavoSutil <4541082348>
2025-09-17 08:57:07 -0300

    feat: adicionando controller de solicitacoes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Http\Controllers\SolicitarDocumentoController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/solicitar-documento', function () {
-    return view('solicitar-documento');
-});
+Route::get('/solicitar-documento', [SolicitarDocumentoController::class, 'index']);
 
 Route::get('/docs/{any?}', function () {
     return redirect('/docs/html/index.html');
 })->where('any', '.*');
+
+Route::redirect('/', '/solicitar-documento');
